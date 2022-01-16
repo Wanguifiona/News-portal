dd2c55387d44a281983dbd5be879c52f45ee07f9
Fiona Wangui <91875682+Wanguifiona@users.noreply.github.com>
2022-01-16 22:40:53 +0300

    add sql class

---
+++ src/test/java/dao/Sql2oDepartmentDaoTest.java
@@ -0,0 +1,8 @@
+//package dao;
+//
+//public class Sql2oDepartmentDaoTest {
+//    private Sql2oDepartmentDao departmentDao;
+//    private Sql2oRestaurantDao restaurantDao;
+//    private Connection conn;
+//
+//}
