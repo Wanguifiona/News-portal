e9e1465b25e52e22c9aca17c4601151021564ee5
Fiona Wangui <91875682+Wanguifiona@users.noreply.github.com>
2022-01-17 00:51:33 +0300

    edit build gradle

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,24 +1,28 @@
-//import static spark.Spark.*;
-//
-//import com.google.gson.Gson;
-//import exceptions.ApiException;
-//import org.sql2o.Connection;
-//
-//import org.sql2o.Connection;
-//import org.sql2o.Sql2o;
-//
-//public class App {
-//    public static void main(String[] args) {
-//        Sql2oDepartmentDao departmentDao;
-//        Sql2oNewsDao newsDao;
-//        Sql2oUserDao userDao;
-//        Connection conn;
-//        Gson gson = new Gson();
-//
-//
-//        restaurantDao = new Sql2oRestaurantDao(sql2o);
-//        foodtypeDao = new Sql2oFoodtypeDao(sql2o);
-//        reviewDao = new Sql2oReviewDao(sql2o);
-//        conn = sql2o.open();
-//    }
-//}
+import static spark.Spark.*;
+
+import com.google.gson.Gson;
+import dao.Sql2oUserDao;
+import exceptions.ApiException;
+import org.sql2o.Connection;
+
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+public class App {
+    public static void main(String[] args) {
+        Sql2oDepartmentDao departmentDao;
+        Sql2oNewsDao newsDao;
+        Sql2oUserDaoao userDao;
+        Connection conn;
+        Gson gson = new Gson();
+
+//        staticFileLocation("/public");
+//        String connectionString = "jdbc:h2:~/jadle.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+//        Sql2o sql2o = new Sql2o(connectionString, "", "");
+
+        restaurantDao = new Sql2oRestaurantDao(sql2o);
+        foodtypeDao = new Sql2oFoodtypeDao(sql2o);
+        reviewDao = new Sql2oReviewDao(sql2o);
+        conn = sql2o.open();
+    }
+}
