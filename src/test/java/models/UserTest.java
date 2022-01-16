6a7ed5363fa9cbcbd74acc5675980c8479850cf4
Fiona Wangui <91875682+Wanguifiona@users.noreply.github.com>
2022-01-16 13:39:53 +0300

    add users test class

--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -1,7 +1,40 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class UserTest {
 
+
+    @Test
+    public void user_instantiatesCorrectly_true() {
+        User user = new User("George");
+        assertTrue(user instanceof User);
+    }
+
+    @Test
+    public void name_instantiatesWith_Name_true() {
+        User user = new User("George");
+        assertEquals("George", user.getName());
+    }
+
+    @Test
+    public void name_createUser() {
+        User user = new User("George");
+        user.createUser("Joe");
+        assertEquals("Joe", user.getName());
+    }
+
+    @Test
+    public void user_namesAreEqual_true() {
+        User firstuser = new User("George");
+        User seconduser = new User("George");
+        assertEquals(true, firstuser.equals(seconduser));
+    }
+
+    // HELPERS
+    public User createUser1(){
+        return new User("George");
+    }
 }
