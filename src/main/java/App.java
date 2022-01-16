0f04c7124a4b33513c8dc637dbbc70b756258bb5
Fiona Wangui <91875682+Wanguifiona@users.noreply.github.com>
2022-01-16 22:17:24 +0300

    add app class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,24 @@
-public class App {
-}
+//import static spark.Spark.*;
+//
+//import com.google.gson.Gson;
+//import exceptions.ApiException;
+//import org.sql2o.Connection;
+//
+//import org.sql2o.Connection;
+//import org.sql2o.Sql2o;
+//
+//public class App {
+//    public static void main(String[] args) {
+//        Sql2oDepartmentDao departmentDao;
+//        Sql2oNewsDao newsDao;
+//        Sql2oUserDao userDao;
+//        Connection conn;
+//        Gson gson = new Gson();
+//
+//
+//        restaurantDao = new Sql2oRestaurantDao(sql2o);
+//        foodtypeDao = new Sql2oFoodtypeDao(sql2o);
+//        reviewDao = new Sql2oReviewDao(sql2o);
+//        conn = sql2o.open();
+//    }
+//}
