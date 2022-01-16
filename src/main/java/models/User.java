795a127e6b13494589d3f3a5191ea1bf731efb07
Fiona Wangui <91875682+Wanguifiona@users.noreply.github.com>
2022-01-16 13:36:02 +0300

    add users class

---
+++ src/main/java/models/User.java
@@ -0,0 +1,37 @@
+package models;
+
+import java.util.Objects;
+
+public class User {
+
+    public String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void createUser(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(getName(), user.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName());
+    }
+}
